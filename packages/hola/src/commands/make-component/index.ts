d76fcacc6f83c094f40fb5307321d607d8968b39
Jake Neal <hello@jakeneal.co.uk>
2025-04-10 23:57:33 +0100

    🐛 Fixed bug when path is defined but defaultPath is not

--- packages/hola/src/commands/make-component/index.ts
+++ packages/hola/src/commands/make-component/index.ts
@@ -64,7 +64,7 @@ export default defineCommand({
         const { path } = args;
         let componentPath = path;
 
-        if (!path || !holaConfig.defaultPath) {
+        if (!path) {
             intro('make:component');
 
             componentPath = await text({
